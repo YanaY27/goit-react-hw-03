bb2f1c207657fc0296e0e6defa5dbb4b23ae1049
YanaY27 <yanayeromina@gmail.com>
2024-05-03 16:13:15 +0300

    .

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,16 @@
+import { Form, Formik } from "formik"
+
+
+const ContactForm = () => {
+  return (
+    <div>
+      <Formik>
+        <Form>
+            
+        </Form>
+      </Formik>
+    </div>
+  )
+}
+
+export default ContactForm
